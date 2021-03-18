5b96c85455b4c1e9ad679cf6a92459e3aff9fef2
totojanin <tomas.janin@mercadolibre.com>
2021-03-18 17:44:52 -0300

    Agrego carpeta de ejercicio de ordenamiento radix

---
+++ EjerciciosPractica1B/src/demo/RadixSortEjerc.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class RadixSortEjerc
+{
+	public static void radixSort(int []arr)
+	{
+		// PROGRAMAR AQUI
+	}
+
+	public static void main(String[] args)
+	{
+		int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
+		radixSort(arr);
+		
+		for(int i=0; i<arr.length;i++)
+		{
+			System.out.print(arr[i]+(i<arr.length-1?",":""));
+		}
+	}
+}
---
+++ EjerciciosPractica1B/src/demo/StringUtil.java
@@ -0,0 +1,47 @@
+package demo;
+
+public class StringUtil
+{
+	// Retorna una cadena compuesta por n caracteres c
+	// Ejemplo: replicate('x',5) ==> 'xxxxx'
+	public static String replicate(char c, int n)
+	{
+		return "";
+	}
+
+	// Retorna una cadena de longitud n, compuesta por s
+	// y precedida de tantos caracteres c como sea necesario
+	// para completar la longitud mencionada
+	// Ejemplo lpad("5",3,'0') ==> "005"
+	public static String lpad(String s, int n, char c)
+	{
+		return "";
+	}
+
+	// Retorna un String[] conteniendo los elementos de arr
+	// representados como cadenas de caracteres
+	public static String[] toStringArray(int arr[])
+	{
+		return null;
+	}
+
+	// Retorna un String[] conteniendo los elementos de arr
+	// representados como cadenas de caracteres
+	public static int[] toIntArray(String arr[])
+	{
+		return null;
+	}
+
+	// Retorna la longitud del elemento con mayor cantidad
+	// de caracteres del array arr
+	public static int maxLength(String arr[])
+	{
+		return 0;
+	}
+
+	// Completa los elemento del arr agregando caracteres c
+	// a la izquierda, dejando a todos con la longitud del mayor
+	public static void lNormalize(String arr[], char c)
+	{
+	}
+}
