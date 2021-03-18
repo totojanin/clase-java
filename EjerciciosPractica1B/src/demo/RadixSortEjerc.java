e40ffaca8b55f3cc8b62119996542ca46d439e81
totojanin <tomas.janin@mercadolibre.com>
2021-03-18 19:32:55 -0300

    Ejercicio de ordenamiendo radix

--- EjerciciosPractica1B/src/demo/RadixSortEjerc.java
+++ EjerciciosPractica1B/src/demo/RadixSortEjerc.java
@@ -1,20 +1,80 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RadixSortEjerc
 {
-	public static void radixSort(int []arr)
+	public static int[] radixSort(int []arr)
 	{
-		// PROGRAMAR AQUI
+		String[] sArr = StringUtil.toStringArray(arr);
+
+		StringUtil.lNormalize(sArr, '0');
+
+		sArr = agregarElementosAListas(sArr);
+
+		return StringUtil.toIntArray(sArr);
+	}
+
+	public static String[] agregarElementosAListas(String[] sArr) {
+		Map<Character, ArrayList<String>> hashMap = new HashMap<>();
+
+		int i = sArr[0].length() - 1;
+
+		while (i >= 0) {
+			for (int j = 0; j < sArr.length; j++) {
+				char c = sArr[j].charAt(i);
+
+				ArrayList list = hashMap.get(c);
+
+				if (list != null) {
+					list.add(sArr[j]);
+				}
+				else {
+					list = new ArrayList();
+
+					list.add(sArr[j]);
+
+					hashMap.put(c, list);
+				}
+			}
+
+			sArr = rearmarArray(hashMap, sArr.length);
+
+			hashMap = new HashMap<>();
+
+			i--;
+		}
+
+		return sArr;
+	}
+
+	public static String[] rearmarArray(Map<Character, ArrayList<String>> hashMap, int arrLength) {
+		String[] sArr = new String[arrLength];
+
+		int i = 0;
+
+		for (Map.Entry<Character, ArrayList<String>> entry : hashMap.entrySet()) {
+			for (String elemento : entry.getValue()) {
+				sArr[i] = elemento;
+
+				i++;
+			}
+		}
+
+		return sArr;
 	}
 
 	public static void main(String[] args)
 	{
-		int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
-		radixSort(arr);
+		int arr[] = { 16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8 };
+
+		arr = radixSort(arr);
 		
-		for(int i=0; i<arr.length;i++)
+		for (int i = 0; i < arr.length; i++)
 		{
-			System.out.print(arr[i]+(i<arr.length-1?",":""));
+			System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
 		}
 	}
 }
--- EjerciciosPractica1B/src/demo/StringUtil.java
+++ EjerciciosPractica1B/src/demo/StringUtil.java
@@ -6,7 +6,13 @@ public class StringUtil
 	// Ejemplo: replicate('x',5) ==> 'xxxxx'
 	public static String replicate(char c, int n)
 	{
-		return "";
+		String palabraReplicada = "";
+
+		for (int i = 0; i < n; i++) {
+			palabraReplicada += c;
+		}
+
+		return palabraReplicada;
 	}
 
 	// Retorna una cadena de longitud n, compuesta por s
@@ -15,33 +21,67 @@ public class StringUtil
 	// Ejemplo lpad("5",3,'0') ==> "005"
 	public static String lpad(String s, int n, char c)
 	{
-		return "";
+		String palabraNueva = s;
+
+		int cantLetrasAgregar = n - s.length();
+
+		if (cantLetrasAgregar > 0) {
+			palabraNueva = replicate(c, cantLetrasAgregar) + palabraNueva;
+		}
+
+		return palabraNueva;
 	}
 
 	// Retorna un String[] conteniendo los elementos de arr
 	// representados como cadenas de caracteres
 	public static String[] toStringArray(int arr[])
 	{
-		return null;
+		String[] sArr = new String[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			Integer aux = arr[i];
+
+			sArr[i] = aux.toString();
+		}
+
+		return sArr;
 	}
 
 	// Retorna un String[] conteniendo los elementos de arr
 	// representados como cadenas de caracteres
 	public static int[] toIntArray(String arr[])
 	{
-		return null;
+		int[] iArr = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			iArr[i] = Integer.parseInt(arr[i]);
+		}
+
+		return iArr;
 	}
 
 	// Retorna la longitud del elemento con mayor cantidad
 	// de caracteres del array arr
 	public static int maxLength(String arr[])
 	{
-		return 0;
+		int maxLong = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].length() > maxLong)
+				maxLong = arr[i].length();
+		}
+
+		return maxLong;
 	}
 
 	// Completa los elemento del arr agregando caracteres c
 	// a la izquierda, dejando a todos con la longitud del mayor
 	public static void lNormalize(String arr[], char c)
 	{
+		int maxLong = maxLength(arr);
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = lpad(arr[i], maxLong, c);
+		}
 	}
 }
